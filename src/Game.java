9115e12c7fdf119d42e0d22876c74030d5536e0d
luanAfons0 <luan.hmatao@gmail.com>
2024-10-12 13:23:32 -0300

    feat: basic game loop and FPS system

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,7 @@
+import entities.GameWindow;
+
 public class Game {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        new GameWindow();
     }
 }
---
+++ src/entities/GameWindow.java
@@ -0,0 +1,24 @@
+package entities;
+
+import javax.swing.JFrame;
+
+public class GameWindow {
+    public static final int WINDOW_WIDTH = 1000;
+    public static final int WINDOW_HEIGHT = 800;
+
+    public GameWindow() {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Space Invaders");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        gamePanel.startGameThread();
+    }
+}
